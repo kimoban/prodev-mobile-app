30b9e6f875437940dfe94ca0f43743d39dc426c3
kimoban <isaiahkimoban87@gmail.com>
2025-09-03 14:37:28 +0000

    chore: add nativewind-env.d.ts and metro.config.js for NativeWind support

---
+++ prodev-mobile-app-0x06/metro.config.js
@@ -0,0 +1,6 @@
+// Learn more: https://docs.expo.dev/guides/using-nativewind/
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
